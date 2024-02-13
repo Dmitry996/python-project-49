76144a7b15a637b22b4e641d8ee0d5c653ff8cdd
dmitry_rodin <dimoon-96@yandex.ru>
2024-02-13 09:06:35 +0700

    ver_0.1.1

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,17 +3,12 @@ from random import randint, choice
 MESSEGE = 'What number is missing in the progression?'
 
 
-def sequence_generator():
+def game():
     begin = randint(1, 50)
     step = randint(2, 10)
     end = begin + step * 11  # Увеление длинный последовательности на 11
 
     result = [str(i) for i in range(begin, end, step)]
-    elem = choice(result)
-    result[result.index(elem)] = '..'
-    return ' '.join(result), elem
-
-
-def game():
-    question, right_answer = sequence_generator()
-    return question, right_answer
+    right_answer = choice(result)
+    result[result.index(right_answer)] = '..'
+    return ' '.join(result), right_answer
